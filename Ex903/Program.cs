52ee3c30111288772c6c98c65903cfedd758166c
Piotrek <@>
2017-12-06 09:21:42 +0100

    przeslanie plikow

---
+++ Ex903/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex903
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            double a;
+            while (true)
+            {
+                try
+                {
+                    a = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            Console.WriteLine("a ={0}", a);
+            Console.ReadKey();
+
+        }
+    }
+}
